f0720bf31c62941b917773f74b3d97fa60b4545b
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-16 14:14:15 -0300

    corrige tela de aplicação

--- src/layouts/Application.vue
+++ src/layouts/Application.vue
@@ -12,13 +12,15 @@ export default {
   <v-app>
     <v-app-bar :elevation="2">
       <v-app-bar-title>To Do List</v-app-bar-title>
-
+      
       <v-spacer></v-spacer>
-
+      
       <v-btn icon @click="goToRegister()">
         <v-icon>mdi-account-multiple-plus </v-icon>
       </v-btn>
     </v-app-bar>
+    
+    <br><br>
 
     <router-view />
   </v-app>
