26d940835649392d21ae3f61a986918cf894c846
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-23 10:32:05 -0300

    fixa log out

--- src/layouts/Application.vue
+++ src/layouts/Application.vue
@@ -1,10 +1,11 @@
 <script>
 
-export default{
-  methods:{
-    logOut(){
+export default {
+  methods: {
+    logOut() {
       localStorage.removeItem("access_token")
-      this.$router.push('/')
+      location.replace("/");
+      //this.$router.push('/')
     }
   }
 }
@@ -15,14 +16,14 @@ export default{
   <v-app>
     <v-app-bar :elevation="2">
       <v-app-bar-title>To Do List</v-app-bar-title>
-      
+
       <v-spacer></v-spacer>
-      
+
       <v-btn>
         <div @click="logOut">Log Out</div>
       </v-btn>
     </v-app-bar>
-    
+
     <br><br>
 
     <router-view />
