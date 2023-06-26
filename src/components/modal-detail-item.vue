d359d9941d96afa2d69e37021d19a3e63f0d33b4
LuizFeller <luiz.feller@terra.com.br>
2023-06-26 09:48:13 -0300

    ditalha itens da lista e atribui ações a ele: resolver, detalhar e apagar

---
+++ src/components/modal-detail-item.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    props:{
+        infos: Object
+    }
+}
+
+</script>
+
+<template>
+    <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
+        <v-card class="w-75 pa-4">
+            <v-card-text> <h3>id:</h3> {{ infos.id }}</v-card-text>
+            <v-card-text> <h3>title:</h3> {{ infos.title }}</v-card-text>
+            <v-card-text> <h3>deadline:</h3> {{ infos.deadline }}</v-card-text>
+            <v-card-text> <h3>done:</h3> {{ infos.done }}</v-card-text>
+            <v-card-text> <h3>listId:</h3> {{ infos.listId }}</v-card-text>
+            <v-card-text> <h3>createdAt:</h3> {{ infos.createdAt }}</v-card-text>
+            <v-card-text> <h3>updatedAt:</h3> {{ infos.updatedAt }}</v-card-text>
+            <v-card-text> <h3>authorId:</h3> {{ infos.authorId }}</v-card-text>
+        </v-card>
+
+    </v-card>
+</template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
