5a922440d4552a9a9e1f4f0c9f54b53f3f22d7d4
LuizFeller <luiz.feller@terra.com.br>
2023-06-26 09:34:53 -0300

    criação de modal para nova lista

---
+++ src/components/modal-new-list.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+    data(){
+        return{
+            listName: '',
+        }
+    },
+    methods: {
+        handleCreateList(){
+            this.$emit('new-list', this.listName)
+        }
+    }
+}
+
+</script>
+
+<template>
+    <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
+        <v-card class="h-25 w-25 pa-4">
+            <v-card-title>Nome da Lista</v-card-title>
+            <v-text-field v-model="listName"></v-text-field>
+            <v-btn @click="handleCreateList">Criar</v-btn>
+        </v-card>
+
+    </v-card>
+</template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
