4fe729b8b5342e3ff3b9f0652765b03eaecf30ce
LuizFeller <luiz.feller@terra.com.br>
2023-06-26 09:46:41 -0300

    padroniza endpoints para chamada de api

--- src/api/todolists.js
+++ src/api/todolists.js
@@ -5,5 +5,16 @@ export const listApiMixin = {
     async list() {
       return await privateApi.get("/lists");
     },
+    //inserido em 23/06/23 (Luiz) - início
+    async detailList(id) {
+      return await privateApi.get(`/lists/${id}`);
+    },
+    async createList(title) {
+      return await privateApi.post("/lists/new", { title: title });
+    },
+    async deleteList(id){
+      return await privateApi.delete(`/lists/${id}`)
+    }
+    // fim
   },
 };
