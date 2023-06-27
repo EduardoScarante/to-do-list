5bc48c1a55f904cce79480837b0c11cd8edfdb0f
LuizFeller <luiz.feller@terra.com.br>
2023-06-27 11:29:41 -0300

    renomeado e migrado para components/modal

---
+++ src/components/modal/edit-name-list.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default {
+    props: {
+        id: String,
+        name: String,
+    },
+    data() {
+        return {
+            newNameList: '',
+        }
+    },
+    methods: {
+        handleUpdateListName() {
+            this.$emit('new-name-list', this.newNameList, this.id)
+        },
+        closeModal() {
+            this.$emit('close-modal')
+        }
+    }
+}
+
+</script>
+
+<template>
+    <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
+        <v-card class="w-50 pa-4">
+            <v-card-subtitle>{{ id }}</v-card-subtitle>
+            <v-card-title>Nome atual da lista: {{ name }}</v-card-title>
+            <v-text-field v-model="newNameList" placeholder="Novo Nome"></v-text-field>
+            <v-action class="d-flex justify-center">
+                <v-btn @click="handleUpdateListName" class="w-25 mx-2">
+                    Atualizar
+                </v-btn>
+                <v-btn @click="closeModal" class="w-25 mx-2">
+                    Fechar
+                </v-btn>
+            </v-action>
+        </v-card>
+    </v-card>
+</template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
---
+++ src/components/modal/new-item.vue
@@ -0,0 +1,42 @@
+<script>
+
+export default {
+    data(){
+        return{
+            itemName: '',
+            deadLineDate: `${new Date().getFullYear()}-0${new Date().getMonth()+1}-${new Date().getDate()}`,
+            deadLineHour: `${new Date().getHours()}:${new Date().getMinutes()}`,     
+        }
+    },
+    methods: {
+        handleCreateList(){
+            const formatedDate = `${this.deadLineDate}T${this.deadLineHour}:00.000Z`
+            this.$emit('new-item', this.itemName, formatedDate)
+        },
+    },
+    
+    }
+
+</script>
+
+<template>
+    <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
+        <v-card class="w-25 pa-4">
+            <v-card-title>Nome da Tarefa</v-card-title>
+            <v-text-field v-model="itemName"></v-text-field>
+            <v-text-field v-model="deadLineDate" type="date" :value="deadLineDate"></v-text-field>
+            <v-text-field v-model="deadLineHour" type="time" :value="deadLineHour"></v-text-field>
+            <v-btn @click="handleCreateList">Criar</v-btn>
+        </v-card>
+    </v-card>
+</template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
