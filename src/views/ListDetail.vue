98986f587a09f96e12211f985e91567e99453ff1
LuizFeller <luiz.feller@terra.com.br>
2023-06-27 11:35:36 -0300

    ajustado as rotas  de detail-item.vue e new-item.vue e inserido o compontente Loading.vue

--- src/views/ListDetail.vue
+++ src/views/ListDetail.vue
@@ -2,13 +2,15 @@
 
 import { listApiMixin } from "@/api/todolists";
 import { toDoItemsApiMixin } from "@/api/todoitems";
-import modalDetail from "@/components/modal-detail-item.vue"
-import ModalNewList from '@/components/modal-new-item.vue'
+import modalDetail from "@/components/modal/detail-item.vue"
+import ModalNewList from '@/components/modal/new-item.vue'
+import Loading from '@/components/Loading.vue'
 
 export default {
     components: {
         modalDetail,
-        ModalNewList
+        ModalNewList,
+        Loading,
     },
     mixins: [listApiMixin, toDoItemsApiMixin],
     data() {
@@ -17,9 +19,12 @@ export default {
             itemDetailInfos: {},
             showModalDetail: false,
             showNewItemForm: false,
+
+            loading: false,
         }
     },
     async mounted() {
+        this.loading = true
         this.getItems()
     },
     methods: {
@@ -28,6 +33,7 @@ export default {
             try {
                 const { data } = await this.detailList(id)
                 this.items = data.items;
+                this.loading = false
             } catch {
                 alert("Deu erro... D:")
             }
@@ -49,9 +55,10 @@ export default {
             this.handleWithError(status)
         },
 
-        async newItem(title) {
+        async newItem(title, deadline) {
+            this.loading = true
             const id = this.$route.params.id
-            const { status } = await this.createItem(title, id)
+            const { status } = await this.createItem(id, title, deadline)
             this.handleWithError(status)
             this.showNewItemForm = false
             this.getItems()
@@ -65,19 +72,37 @@ export default {
             } else {
                 alert("Deu erro")
             }
+            this.loading = false
         },
 
         /* METODO DE ESTILIZAÇÂO */
         btnColor(a) {
-            console.log(a);
-            return a == true ? 'success' : 'error'
+            return a == true ? 'rgb(144, 238, 144)' : 'rgb(240, 128, 128)'
         },
+        /* FORMATAÇÂO DA DATA */
+        formatDate(date) {
+            const pureDate = date.split("T")[0].split("-").reverse().join("/")
+            const pureHour = date.split("T")[1].split(".")[0]
+            return `${pureDate} - ${pureHour}`
+        },
+    },
+    computed: {
+        organizeDeadlineDate() {
+            if (!this.items) return ''
+            const ordened = this.items.sort((a, b) => new Date(a.deadline) - new Date(b.deadline))
+            console.log(ordened);
+            return ordened.filter(elemento => elemento.done == false).concat(ordened.filter(elemento => elemento.done == true))
+      }
     }
 }
 
 </script>
 
 <template>
+     <span @click="this.$router.go(-1)" class="c-pointer pa-4 material-symbols-outlined">
+        arrow_back_ios_new
+    </span>
+
     <v-card>
         <v-card class="w-100 d-flex justify-center pa-4">
             <v-btn @click="showNewItemForm = true">
@@ -85,12 +110,13 @@ export default {
             </v-btn>
         </v-card>
 
-        <v-card v-for="item in items">
-            <v-card class="d-flex align-center ma-2 elevation-2">
-                <v-card-title>{{ item.title }}</v-card-title>
-                <v-card-text>{{ item.done }}</v-card-text>
+       <v-card v-for="item in organizeDeadlineDate">
+            <v-card class="d-flex align-center ma-2 elevation-2" :color="btnColor(item.done)">
+                <v-card :title=formatDate(item.deadline) :color="btnColor(item.done)"></v-card>
+                <v-card-title class="w-25">{{ item.title }}</v-card-title>
+                <v-card-text class="text-white">.</v-card-text>
                 <v-card-actions>
-                    <v-btn @click="handleResolveItem(item.id)">MARCAR COMO RESOLVIDO</v-btn>
+                    <v-btn @click="handleResolveItem(item.id)" v-show="!item.done">MARCAR COMO RESOLVIDO</v-btn>
                     <v-btn @click="handleShowDetail(item.id)">VER DETALHES</v-btn>
                     <v-btn @click="handleDeleleItem(item.id)">APAGAR</v-btn>
                 </v-card-actions>
@@ -101,5 +127,15 @@ export default {
 
     <ModalNewList v-if="showNewItemForm" @new-item="newItem"></ModalNewList>
 
-    <modalDetail v-if="showModalDetail" :infos="itemDetailInfos" @click="showModalDetail = false"></modalDetail>
+    <!-- MODAL DE DETALHE DO ITEM -->
+    <modalDetail v-if="showModalDetail" :infos="itemDetailInfos" @closeModal="this.showModalDetail = false"></modalDetail>
+    <!-- MODAL DE LOADING -->
+    <Loading v-if="loading"></Loading>
+
 </template>
+
+<style>
+.c-pointer {
+    cursor: pointer;
+}
+</style>
