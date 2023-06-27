cf56651449f5dc03469c1d9461dc7b55bb8e9f2e
LuizFeller <luiz.feller@terra.com.br>
2023-06-27 11:30:29 -0300

    criado o Loading.vue

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card class="loading d-flex align-center justify-center">
+        <v-progress-circular indeterminate color="amber"></v-progress-circular>
+    </v-card>
+</template>
+
+<style>
+.loading {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    
+    top: 0px;
+    left: 0;
+    background: rgb(0, 0, 0, 0.2);
+}
+</style>
