139cadfdd49038eebdb9a81b20ee459ac720ee09
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-30 14:57:39 -0300

    Corrige título

--- src/components/modal/edit-name-list.vue
+++ src/components/modal/edit-name-list.vue
@@ -30,7 +30,7 @@ export default {
 <template>
     <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
         <v-card class="w-25 pa-4">
-            <v-card-title>new list name</v-card-title>
+            <v-card-title>New name list</v-card-title>
             <v-text-field :label="name" v-model="newNameList" placeholder="Novo Nome" :rules="inputRule"></v-text-field>
             <v-action class="d-flex justify-center">
                 <v-btn @click="closeModal" class="w-25 mx-2">
